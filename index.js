9e1c0f375ee3b25892c17c615ed208d5970e3de0
vinaykanth-s <vinaykanth.edu@gmail.com>
2021-10-24 22:43:16 +0530

    first version

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Vinay Kanth - Frontend Engineer at Askadmissions.ai
+
+A passionate software engineer and a problem solver 
+with expertise in Frontend development using modern technologies 
+such as JavaScript, React, Material UI, HTML/CSS.
+
+Twitter: https://twitter.com/VinaykanthS
+
+`);
