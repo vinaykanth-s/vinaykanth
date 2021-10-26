4b3b175592e093b426ba3f0380044cdaacf5a8cf
vinaykanth-s <vinaykanth.edu@gmail.com>
2021-10-26 08:48:33 +0530

    clear console implemented using clear-any-console package

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+const clearConsole = require("clear-any-console");
+clearConsole();
+
+// console.clear();
+
 console.log(`
 Vinay Kanth - Frontend Engineer at Askadmissions.ai
 
 A passionate software engineer and a problem solver 
-with expertise in Frontend development using modern technologies 
+with expertise in Frontend development using modern technologies
 such as JavaScript, React, Material UI, HTML/CSS.
 
 Twitter: https://twitter.com/VinaykanthS
